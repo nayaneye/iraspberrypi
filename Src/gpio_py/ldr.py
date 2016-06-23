9954fd88af8cac8ee1037ca3a516bbb4a80b0734
nayaneye <nayaneye@gmail.com>
2016-06-23 02:15:54 +0000

    ldr path

--- Src/gpio_py/ldr.py
+++ Src/gpio_py/ldr.py
@@ -7,26 +7,26 @@ GPIO.setwarnings(False)
 
 DEBUG = 1
 GPIO.setmode(GPIO.BCM)
- 
+
 def RCtime (RCpin):
         reading = 0
         GPIO.setup(RCpin, GPIO.OUT)
         GPIO.output(RCpin, GPIO.LOW)
         time.sleep(.1)
- 
+
         GPIO.setup(RCpin, GPIO.IN)
         # This takes about 1 millisecond per loop cycle
         while (GPIO.input(RCpin) == GPIO.LOW):
                 reading += 1
         return reading
- 
-while True:                                     
+
+while True:
 		GetDateTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 		LDRReading = RCtime(3)
 		print RCtime(3)
 
 		# Open a file
-		fo = open("/home/pi/Desktop/gpio_python_code/foo.txt", "wb")
+		fo = open("/home/pi/iraspberrypi/Src/gpio_py/foo.txt", "wb")
 		fo.write (GetDateTime)
 		LDRReading = str(LDRReading)
 		fo.write ("\n")
@@ -35,4 +35,4 @@ while True:
 		# Close opend file
 		fo.close()
 		time.sleep(1)
-		
+
